50b775ebde47a713124f61a2118a04f577b6cc2a
glooom86 <94992697+glooom86@users.noreply.github.com>
2022-03-01 19:55:10 +0500

    Initial commit

---
+++ src/test/java/com/example/PP_311_SpringBoot/Pp311SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PP_311_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
